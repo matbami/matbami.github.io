a744b47d4da4f65cd0767d70d22331a49aa6b8ce
Ayobami <adelekeayobami13@gmail.com>
2020-05-13 18:57:10 +0100

    Another commit

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ const scored = document.getElementById("score");
 
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
+var j = 0;
+var i = 0;
 let score = 0;
 
 let shuffleQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
+// i++;
+// console.log(i);
+
 nextButton.addEventListener("click", () => {
+  // i++;
+  // console.log(i);
   currentQuestionIndex++;
   setNextQuestion();
+  // i++;
+  // console.log(i);
+  // if (i == 5) mode();
 });
 function startGame() {
+  // i++;
+  // console.log(i);
   startButton.classList.add("hide");
   shuffleQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
@@ -24,11 +36,17 @@ function startGame() {
 
 function setNextQuestion() {
   resetState();
+  // i++;
+  // console.log(i);
+  // if (i == 10) {
+  //   mode();
+  // }
   showQuestion(shuffleQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
   questionElement.innerText = question.question;
+
   question.answers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerText = answer.text;
@@ -37,8 +55,15 @@ function showQuestion(question) {
       button.dataset.correct = answer.correct;
     }
     button.addEventListener("click", selectAnswer);
+
+    // if (i == 5) {
+    //   mode();
+    // }
+    // scored.innerText = "";
+
     answerButtonsElement.appendChild(button);
   });
+  // startButton.classList.remove("hide");
 }
 
 function resetState() {
@@ -51,34 +76,99 @@ function resetState() {
 function selectAnswer(e) {
   const selectedButton = e.target;
   const correct = selectedButton.dataset.correct;
+  console.log(correct);
+  // i++;
+  // console.log(i);
+
+  // console.log(i);
+
   if (correct) {
     score++;
-    // const button3 = document.createElement("button");
-    // button3.innerText = score;
-    // console.log(score);
-    scored.innerText = "Your Score " + score;
-  } else {
-    scored.innerText = "Your Score " + score;
   }
 
-  setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach((button) => {
-    setStatusClass(button, button.dataset.correct);
+    if (correct) {
+      setStatusClass(selectedButton, correct);
+      button.disabled = true;
+
+      scored.innerText = "Your Score " + score;
+    } else {
+      setStatusClass(selectedButton, correct);
+      scored.innerText = "Your Score " + score;
+      if (button.dataset.correct) {
+        setStatusClass(button, button.dataset.correct);
+        // setStatusClass(button, button.dataset.correct)
+      } else {
+        button.disabled = true;
+      }
+    }
   });
+  // i += 2;
+  // console.log(i);
+  // if (i == 16) {
+  //   mode();
+  // }
+
   if (shuffleQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide");
+    // i++;
+    // console.log(i);
+    // console.log(shuffleQuestions.length);
+    // console.log(currentQuestionIndex + 1);
+    // console.log(currentQuestionIndex + 2);
+    // if (currentQuestionIndex + 1 == 5) {
+    //   // document.getElementById("myModal").classList.remove("hide");
+
+    // }
+    // console.log(i);
+
+    // if (i == 9) {
+    //   mode();
+    // }
   } else {
+    // resetState();
+
+    // questionContainerElement.classList.add("hide");
+    questionElement.innerText = "you scored " + score + "/5";
+
+    // "Congratulations!! you scored" + score + "/5";
+
+    scored.classList.add("hide");
+    // i++;
+    console.log();
     startButton.innerText = "Restart";
+    // mode(startButton);
+    // if (i == 9) {
+    //   mode();
+    // }
+
     startButton.classList.remove("hide");
+    score = 0;
+    scored.innerText = "";
   }
-  nextButton.classList.remove("hide");
+  scored.classList.remove("hide");
+  // startButton.addEventListener("click", startGame);
+  // startButton.classList.add("hide");
+  // nextButton.classList.remove("hide");
 }
+
+// startButton.classList.remove("hide");
 
 function setStatusClass(element, correct) {
   clearStatusClass(element);
+
   if (correct) {
+    // element.classList.contains("true").className = "correct";
+    // element.addEventListener("click", (event) => {
+    //   // if the clicked element has a class named check
+    //   if (event.target.classList.contains("true")) {
+    //     // remove or add the class checked only from this element
     element.classList.add("correct");
-  } else {
+    // console.log(element.classList)
+  }
+  // });
+  // }
+  else {
     element.classList.add("wrong");
   }
 }
@@ -138,3 +228,32 @@ const questions = [
     ],
   },
 ];
+
+function mode(ja) {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  // modal.classList.remove("hide");
+
+  // Get the button that opens the modal
+  // var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  ja.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
